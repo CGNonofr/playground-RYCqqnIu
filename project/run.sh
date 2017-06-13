690f615cc4a0d5718b7c953aa7ac09a8c3ea090b
Loïc Mangeonjean <loic@codingame.com>
2017-06-13 18:13:48 +0200

    test

--- project/run.sh
+++ project/run.sh
@@ -1,21 +1,3 @@
 #!/bin/bash
 echo "CG> terminal -s bash"
-shellPid=""
-re='^[0-9]+$'
-while ! [[ $shellPid =~ $re ]]; do
-	sleep 1
-	shellPid=`ps -U demo -o pid= | xargs`
-done
-
-echo 'Try to create the file /tmp/result'
-while [ ! -f /tmp/result ]
-do
-  sleep 1
-done
-echo 'Yeah, now write "toto" in it'
-while ! grep "toto" /tmp/result ; do
-	sleep 1
-done 
-echo 'Congratulations'
-echo "CG> success"
-sleep 2
+sleep 1000
